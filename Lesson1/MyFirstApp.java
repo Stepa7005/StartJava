2af4bed065cffef2101555c94260ee39c4ec5724
Stepan <kuntsev.s70@gmail.com>
2021-05-03 01:29:33 +0500

    start

---
+++ Lesson1/MyFirstApp.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte RAM=6;
+        short CPU=4;
+        int DDR=4;
+        long ROM=500;
+        float CPUFRQ=1.2;
+        double CPUFRQ1=1.2*1000000000;
+        char USB=2;
+        Boolean touchpad=true;
+        System.out.println(RAM \n CPU \n);
+    }
+}
---
+++ Lesson1/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte RAM=6;
+        short CPU=4;
+        int DDR=4;
+        long ROM=500;
+        double CPUFRQ=1.2;
+        double CPUFRQ1=1.2*1000000000;
+        char USB=2;
+        Boolean touchpad=true;
+        System.out.print(CPU);
+    }
+}
